1ba3db9023613ccada78faa4a70e439802647c57
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2022-12-11 15:09:54 +0000

    Day 1 complete

--- day_1/day_1_advent.py
+++ day_1/day_1_advent.py
@@ -23,7 +23,23 @@ def top_cal_elf():
         
     print("Max cal is: ", max_cal)
         
-
-
-
-def top_cal_elf():
+def top_three_cal_combined():
+    lines = get_lines()
+    max_list = [0,0,0]
+    current = 0
+    for line in lines:
+        print(line, len(line.strip()))
+        line = line.strip()
+        if len(line) == 0:
+            current = 0
+        else:
+            line = int(line)
+            current += line
+            if current > max_list[0]:
+                max_list[0] = current
+        max_list = sorted(max_list)
+    print(sum(max_list))
+    # print("Max cal is: ", max_cal)
+    
+    
+top_three_cal_combined()

7969253535ef5186eb3205c0df53425c8871d67b
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2022-12-11 18:05:00 +0000

    Day 4 part 1 works now

--- day_4/advent.py
+++ day_4/advent.py
@@ -1,30 +1,24 @@
 # 44:41
 
 def get_lines():
-    # file = open("input.txt", "r")
-    file = open("example.txt", "r")
+    file = open("input.txt", "r")
+    # file = open("example.txt", "r")
     lines = file.readlines()
     return lines
 
+
+def check_range(range_one, range_two):
+    start_one, end_one = map(int, range_one.split('-'))
+    start_second, end_second = map(int, range_two.split('-'))
+    print("Comarping ", start_one , end_one, start_second, end_second)
+    return start_second <= start_one and end_one <= end_second
 def main():
     lines = get_lines()
     count = 0
     for line in lines:
         line = line.strip()
-        from_one = line[0]
-        to_one = line[2]
         lis = line.split(',')
-        first = lis[0].split('-')
-        second = lis[1].split('-')
-        print(first, second)
-        
-        # 2-8,3-7
-        if first[0] <= second[0] and first[1] >= second[1]:
-            
-            print("one contains the other")
-            count += 1
-        elif first[0] >= second[0] and first[1] <= second[1]:
-            print("One contaisn the other")
+        if check_range(lis[0], lis[1]) or check_range(lis[1], lis[0]):
             count += 1
     print(count)
     

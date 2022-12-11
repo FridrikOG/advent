4249e76bc93baaf7db7bf1d11601765ed9a6da76
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2022-12-11 18:15:41 +0000

    day 4 done

--- day_4/advent.py
+++ day_4/advent.py
@@ -1,4 +1,4 @@
-# 44:41
+# 28:21
 
 def get_lines():
     file = open("input.txt", "r")
@@ -12,7 +12,7 @@ def check_range(range_one, range_two):
     start_second, end_second = map(int, range_two.split('-'))
     print("Comarping ", start_one , end_one, start_second, end_second)
     return start_second <= start_one and end_one <= end_second
-def main():
+def complete_overlap():
     lines = get_lines()
     count = 0
     for line in lines:
@@ -21,11 +21,28 @@ def main():
         if check_range(lis[0], lis[1]) or check_range(lis[1], lis[0]):
             count += 1
     print(count)
-    
+
+def check_range_any(range_one, range_two):
+    ''' Check if range one has any overlap with range two'''
+    start_one, end_one = map(int, range_one.split('-'))
+    start_second, end_second = map(int, range_two.split('-'))
+    # 6-6,4-6
+    return start_one <= end_second and end_one >= start_second 
+
+def any_overlap():
+    lines = get_lines()
+    count = 0
+    for line in lines:
+        line = line.strip()
+        lis = line.split(',')
+        if check_range_any(lis[0], lis[1]) or check_range_any(lis[1], lis[0]):
+            count += 1
+    print(count)
     
 # Part 1
-# two_comp_total()
+# complete_overlap()
 # Part 2
-main()
+any_overlap()
+
 
 

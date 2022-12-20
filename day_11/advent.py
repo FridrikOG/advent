cd409722325f4f7e5959ddb1bc4fcb4d1b30b4d2
Friðrik Ö. Gunnarsson <36244964+FridrikOG@users.noreply.github.com>
2022-12-20 18:26:39 +0000

    day 10 done, need to submit input, day 11 monkeys are now in data structure (dictionary)

---
+++ day_11/advent.py
@@ -0,0 +1,52 @@
+# 28:21
+def get_lines():
+    # file = open("input.txt", "r")
+    file = open("example.txt", "r")
+    lines = file.readlines()
+    return lines
+
+def get_monkeys(lines):
+    monkey_instr = []
+    monkey_lis = []
+    for line in lines:
+        line = line.strip(" ")
+        line = line.strip("\n")
+        line = line.strip(",")
+        monkey_instr.append(line.split(' '))                
+        if line == '':
+            print(monkey_instr)
+            items = monkey_instr[1]
+            operation = monkey_instr[2]
+            first_action = operation[3]
+            sign = operation[4]
+            second_action = operation[5]
+            tot_items = []
+            for i in range(2, len(items)):
+                print(i)
+                item  = items[i].strip(',')
+                tot_items.append(item)
+           
+            monkey = {
+            "items" : tot_items,
+            "operations" : [first_action,sign,second_action],
+            }
+            monkey_lis.append(monkey)
+            monkey_instr = []
+    return monkey_lis
+
+def main():
+    lines = get_lines()    
+    total_worry = 0
+    monkeys = get_monkeys(lines)
+    for monkey in monkeys:
+        print(monkey)
+
+    
+
+
+# Part 1
+# complete_overlap()
+# Part 2
+main()
+
+
--- day_8/advent.py
+++ day_8/advent.py
@@ -1,149 +1,45 @@
-# 28:21
-def check_right_side(lines, lis ):
-    for line in lines:
-        line = line.strip('')
-        line = line.strip('\n')
-        for i in range(len(line)):              
-            guard = False
-            for l in range(i):
-                print("Tree being checked ", i)
-                print("Checking ", line[i] , "and ", line[l])
-                
-                if line[i] > line[l]:
-                    guard = True
-                    break
-            if guard == True:
-                lis.append(line[i])
-                guard= False
-    return lis
-def check_edges(lines,lis):
-    for line in lines:
-        line = line.strip('')
-        line = line.strip('\n')
 
 def get_lines():
-    # file = open("input.txt", "r")
-    file = open("example.txt", "r")
-    lines = file.readlines()
+    file = open("input.txt", "r")
+    # file = open("example.txt", "r")
+    lines = [row.strip() for row in file.readlines()]
     return lines
-def check_left_side(lines, lis ):
-    lis = []
-    for line in lines:
-        line = line.strip('')
-        line = line.strip('\n')
-        for i in range(len(line)):              
-            guard = True
-            count = 0
-            for l in range(i):
-                if count == (l+1): 
-                    if line[i] > line[l]:
-                        count += 1
-                else:
-                    guard = False
-            if guard:
-                lis.append(line[i])
-        
-        print("Total ", lis)
-                
-
-
-                # rev = l - len(line) # 0 - 6 = -6
-                # print("The rev index ", rev)
-                # if line[i] > line[rev]:
-                #     lis.append(line[i])
-                #     break
-
-    print("Total ", len(lis))
-    return lis
-
-
                 
 def main():
     lines = get_lines()
-    lis = []
-    total = 0
+    cycle = 0
+    x = 1
+    check_cycle = 20
+    the_sum = 0
+    times_add = 0
+    times_cycle_hit = 0
     for line in lines:
-        line_len = len(line)
-        line = line.strip(" ")
-        line = line.strip("\n")
-        lis.append(line)
-    y = 0
-    for x in range(len(lis)):
-        #Check first column
-        totalBefore = total
-        for y in range(len(lis[x])):
-            # if x == 0 or x == (len(lis) -1):
-                # total += 1
-                # break
-            # Check left, should be 11 cases in the test sample
-            left_guard = True 
-            right_guard = True
-            up_guard = True
-            down_guard = True
-            for i in range(y):
-                # Check from left, 11 cases
-                if not (lis[x][y] > lis[x][i]):
-                    left_guard = False
-                rev = i - len(lis[x])
-                # Check from right, 11 cases 
-                if not (lis[x][y]) < lis[x][rev]:
-                    right_guard = False
-                # Check above
-                up_guard = True
-                down_guard = True
-                if x == 0:
-                    break
-                # Where x is the lines of trees above
-                for j in range(x):
-                    # if tree being checked is not larger than line j with the same y index
-                    if not (lis[x][y] > lis[j][y]):
-                        up_guard = False
-                # Check trees below
-                for h in range(len(lis) - (x) ):
-                    rev = h - x 
-                    # print("x ", x , "Rev ",rev )
-                    if x == 4 and y == 2:
-                        print("Checking last row")
-                        print(len(lis) - x  )
-                        print("ALl guards")
-                        print( left_guard , right_guard, up_guard, down_guard)
-                    if x == len(lis):
-                        pass
-
-                    elif not (lis[x][y] > lis[rev][y]):
-                        down_guard = False
-                    
-
-
-            if left_guard or right_guard or up_guard or down_guard: 
-                total += 1
-            left_guard = True
-            right_guard = True
-            up_guard = True 
-            down_guard = True
-            # Should be 21 cases in total for test
-            
-        print('Amount in ', x, "a:  ", total-totalBefore )
-    
-
-    print("total ", total)
-        # Check above
+        instr = line.split(' ')
+        if instr[0] ==  'noop':
+            cycle_through = 1
+        else:
+            cycle_through = 2
+        for i in range(1,cycle_through+1):
+            if check_cycle == cycle:
+                times_cycle_hit += 1
+                the_sum = the_sum +  (x * check_cycle )
+                print("Adding ", x * check_cycle)
+                check_cycle += 40
                 
+            if i == 1:
+                cycle += 1
+            elif i == 2:
+                x += int(instr[1])
+                times_add += 1
+                cycle +=1
+            
+            
 
+    print(times_add)       
+    print(the_sum)
+    print(times_cycle_hit)
 
-
-        
-    print(lis)
-    # lis = check_right_side(lines, lis)
-    # print(lis)
-    # print(len(lis))
-
-    
-
-
-# Part 1
-# complete_overlap()
-# Part 2
 main()
+
 
 
